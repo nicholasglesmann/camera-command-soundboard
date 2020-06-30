1b719be29cb69dd97ae1d3052d806a4f7dc9919c
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-06-29 17:44:28 -0700

    Finish basic soundboard functionality, add voice picker, add John Mode

--- app.js
+++ app.js
@@ -1,52 +1,242 @@
-let commands;
+let commandsConfig;
+let currentVoice;
+let johnMode;
 
-window.onload = function() {
+let audioPlaying = false;
+let audioStack = [];
 
+let inDevelopment = true;
+
+
+/*
+    VARIABLE DEFINITIONS:
+
+    commandsConfig: A JSON file containing all commands, organized by category
+
+    commandCategory: Groups of similar types of commands
+    
+    command: An individual command within a larger commandCategory
+
+*/
+
+window.onload = function () {
+
+    // Load the JSON before building the page
     fetch("./commands.json")
-    .then(response => response.json())
-    .then(json => commands = json)
-    .then(() => {
-        init();
-    });
+        .then(response => response.json())
+        .then(json => commandsConfig = json)
+        .then(() => {
+            init();
+        });
 
 };
 
 function init() {
 
-    for(let commandType in commands) {
+    loadVoices();
 
-        let commandArray = commands[commandType];
+    renderCommands();
 
-        let commandTypeContainer = document.createElement('div', { id: commandType + '-container' });
+}
 
-        commandTypeContainer = createCommands(commandArray, commandTypeContainer);
-    
+
+function loadVoices() {
+    // list of languages is probably not loaded, wait for it
+    if (window.speechSynthesis.getVoices().length == 0) {
+        window.speechSynthesis.addEventListener('voiceschanged', renderVoicePicker);
+    }
+    else {
+        // languages list available, no need to wait
+        renderVoicePicker()
+    }
+}
+
+
+function renderVoicePicker() {
+
+    // remove voices changed listener
+    window.speechSynthesis.removeEventListener('voiceschanged', renderVoicePicker);
+
+    // get all voices that browser offers
+    let available_voices = window.speechSynthesis.getVoices();
+
+    // set default voice to first available
+    currentVoice = available_voices[0];
+
+
+    let voicePickerContainer = buildContainer('voice-picker', 'Voice Selection');
+
+    let voicePicker = document.createElement('select');
+
+    voicePicker.onchange = (e) => {
+        let value = e.target.value;
+
+        currentVoice = window.speechSynthesis.getVoices().filter(voice => voice.name === value)[0];
+
+    };
+
+    available_voices.forEach(voice => {
+
+        let voiceOption = document.createElement('option');
+        voiceOption.value = voice.name;
+        voiceOption.text = voice.name;
+
+        voicePicker.appendChild(voiceOption);
+
+    });
+
+    voicePickerContainer.appendChild(voicePicker);
+
+
+
+    let johnModeSwitch = buildJohnModeSwitch();
+
+    voicePickerContainer.appendChild(johnModeSwitch);
+
+    document.body.appendChild(voicePickerContainer);
+
+}
+
+function buildJohnModeSwitch() {
+    let johnModeSwitchContainer = buildContainer('john-mode', 'John Mode');
+
+    let johnModeSwitch = document.createElement('input');
+    johnModeSwitch.type = 'checkbox';
+    johnModeSwitch.onchange = (e) => {
+        johnMode = e.target.checked;
+
+        log(johnMode);
+    }
+
+    johnModeSwitchContainer.appendChild(johnModeSwitch);
+
+    return johnModeSwitchContainer;
+}
+
+function buildContainer(id, headingText) {
+
+    let container = document.createElement('div');
+    container.Id = id + '-container';
+
+    let heading = document.createElement('h2');
+    heading.innerText = headingText;
+
+    container.appendChild(heading);
+
+    return container;
+}
+
+
+
+
+function renderCommands() {
+
+    for (let commandCategory in commandsConfig) {
+
+        let commands = commandsConfig[commandCategory];
+
+        let commandCategoryContainer = buildCommandCategoryContainer(commands, commandCategory);
+
+        document.body.appendChild(commandCategoryContainer);
+
     }
 
 }
 
 
-function createCommands(commandArray, commandTypeContainer) {
+function buildCommandCategoryContainer(commands, commandCategory) {
 
-    commandArray.forEach( command => {
+    let commandCategoryContainer = document.createElement('div');
+    commandCategoryContainer.id = commandCategory + '-container';
+
+    let commandCategoryHeading = document.createElement('h2');
+    commandCategoryHeading.innerText = commandCategory;
+
+    commandCategoryContainer.appendChild(commandCategoryHeading);
+
+    commands.forEach(command => {
 
         let commandName = command.name;
         let commandAudioPath = command.path;
 
+        // Create div container for the command
         let commandContainer = document.createElement('div');
         commandContainer.setAttribute('id', commandName);
 
-        // let commandButton = document.createElement('button', {  })
 
-        console.log(commandName);
-        console.log(commandAudioPath);
-        console.log(commandContainer);
+        // Create button to trigger command
+        let commandButton = document.createElement('button');
+        commandButton.setAttribute('value', commandName);
+        commandButton.setAttribute('data-path', commandAudioPath);
+        commandButton.onclick = (e) => {
+            let commandName = e.target.value;
+            let path = e.target.dataset.path;
+            speak(commandName, path);
+        };
+        commandButton.innerText = commandName;
+
+        commandCategoryContainer.appendChild(commandButton);
 
     });
+
+    return commandCategoryContainer;
+
+}
+
+function speak(commandName, path) {
+    log(commandName);
+    log(path);
+
+    if (johnMode) {
+        playAudio(path);
+    } else {
+        synthesizeAudio(commandName);
+    }
+}
+
+
+function playAudio(path) {
+
+    // If audio is currently playing, put the new path into a que
+    if (audioPlaying) {
+
+        audioStack.push(path);
+
+    } else {
+
+        // Play the audio
+        let commandAudio = new Audio(path);
+        audioPlaying = true;
+        commandAudio.play();
+
+        // After the audio finishes, recursively call playAudio if
+        // there are other clips to be played
+        commandAudio.onended = () => {
+            audioPlaying = false;
+
+            if (audioStack.length > 0) {
+                playAudio(audioStack.shift());
+            }
+        };
+    }
+}
+
+
+function synthesizeAudio(commandName) {
+
+    // new SpeechSynthesisUtterance object
+    let utter = new SpeechSynthesisUtterance();
+    utter.rate = 1;
+    utter.pitch = 1;
+    utter.text = commandName;
+    utter.voice = currentVoice;
+
+    // speak
+    window.speechSynthesis.speak(utter);
 
 }
 
 
-function playAudio(command) {
-    console.log(command);
+window.log = function (message) {
+    console.log(message);
 }
